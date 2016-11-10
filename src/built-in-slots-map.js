5b44d4a39f3aec59496b85fe396aed9b968bfbab
Martin <xtools-at@users.noreply.github.com>
2016-11-10 13:26:25 +0100

    Added new built-in Slot Types for Europe
    Added new Slot Types for Europe, Great Britain, Germany and Austria

--- src/built-in-slots-map.js
+++ src/built-in-slots-map.js
@@ -8,5 +8,14 @@ module.exports = {
     'USCity': 'AMAZON.US_CITY',
     'USFirstname': 'AMAZON.US_FIRST_NAME',
     'USState': 'AMAZON.US_STATE',
-    'Literal': 'AMAZON.LITERAL',
+    'ATCity': 'AMAZON.AT_CITY',
+    'ATRegion': 'AMAZON.AT_REGION',
+    'DECity': 'AMAZON.DE_CITY',
+    'DEFirstname': 'AMAZON.DE_FIRST_NAME',
+    'DERegion': 'AMAZON.DE_REGION',
+    'GBCity': 'AMAZON.GB_CITY',
+    'GBFirstname': 'AMAZON.GB_FIRST_NAME',
+    'GBRegion': 'AMAZON.GB_REGION',
+    'EUCity': 'AMAZON.EUROPE_CITY',
+    'Literal': 'AMAZON.LITERAL'
 };
